129b88bc4469b7eb43ac8c57762f4fbf54ed8df1
AramZakaryan <aram.h.zakaryan@gmail.com>
2024-05-23 17:39:50 +0400

    res niveau5

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,9 @@
 import { BadRequestException, Injectable } from '@nestjs/common'
 import { CreateUserDto } from './dto/create-user.dto'
-import { UpdateUserDto } from './dto/update-user.dto'
 import { InjectRepository } from '@nestjs/typeorm'
 import { User } from './entities/user.entity'
 import { Repository } from 'typeorm'
+import * as argon2 from 'argon2'
 
 @Injectable()
 export class UserService {
@@ -13,11 +13,13 @@ export class UserService {
     // Check if a user with the same email already exists
     const existUser = await this.userRepository.findOneBy({ email: createUserDto.email })
     if (existUser) throw new BadRequestException('user exists')
-    const user = await this.userRepository.save({
-      email: createUserDto.email,
-      password: '',
-    })
-    return user
+    const user = await this.userRepository
+      .save({
+        email: createUserDto.email,
+        password: await argon2.hash(createUserDto.password),
+      })
+      .catch((err) => new BadRequestException(err))
+    return { user }
   }
 
   findOne(id: number) {
